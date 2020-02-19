397d5b8a8a1e84aa0d8146cf5828fdac95e22004
Boris <jackofdiamond596@gmail.com>
2020-02-19 18:04:45 +0200

    refactor(igxMask): cleanup/streamline parsing and input handling
    - input parsing go through one method
    - add drag and drop functionality
    - set default value on mask prop

--- projects/igniteui-angular/src/lib/date-picker/date-picker.component.spec.ts
+++ projects/igniteui-angular/src/lib/date-picker/date-picker.component.spec.ts
@@ -1,6 +1,6 @@
 import { Component, ViewChild, ElementRef } from '@angular/core';
 import { async, fakeAsync, TestBed, tick, flush, ComponentFixture } from '@angular/core/testing';
-import { FormsModule, FormGroup, FormBuilder, FormControl, ReactiveFormsModule} from '@angular/forms';
+import { FormsModule, FormGroup, FormBuilder, FormControl, ReactiveFormsModule } from '@angular/forms';
 import { By } from '@angular/platform-browser';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { IgxDatePickerComponent, IgxDatePickerModule } from './date-picker.component';
@@ -206,7 +206,7 @@ describe('IgxDatePicker', () => {
                 expect(input).toEqual(document.activeElement);
             }));
 
-            it('When a modal datepicker is closed via outside click, the focus should remain on the input',
+        it('When a modal datepicker is closed via outside click, the focus should remain on the input',
             fakeAsync(() => {
                 const datePickerDom = fixture.debugElement.query(By.css('igx-date-picker'));
                 let overlayToggle = document.getElementsByClassName('igx-overlay__wrapper--modal');
@@ -230,7 +230,7 @@ describe('IgxDatePicker', () => {
                 expect(input).toEqual(document.activeElement);
             }));
 
-            it('When datepicker is closed upon selecting a date, the focus should remain on the input',
+        it('When datepicker is closed upon selecting a date, the focus should remain on the input',
             fakeAsync(() => {
                 const datePickerDom = fixture.debugElement.query(By.css('igx-date-picker'));
                 let overlayToggle = document.getElementsByClassName('igx-overlay__wrapper--modal');
@@ -1149,7 +1149,8 @@ describe('IgxDatePicker', () => {
             expect(input).toBeDefined();
             datePicker.isSpinLoop = false;
 
-            input.nativeElement.focus();
+            input.triggerEventHandler('focus', {});
+            fixture.detectChanges(); // bound transformedDate assign
             UIInteractions.sendInput(input, '31-03-19');
             expect(input.nativeElement.value).toBe('31-03-19');
 
